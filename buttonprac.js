4b42eb948deb7a5fded241fa1d51ca5533099265
nakeopilavan <nakeopilavan@s.sfusd.edu>
2019-03-11 17:54:16 -0700

    update

--- buttonprac.js
+++ buttonprac.js
@@ -1,14 +1,25 @@
-var value = 0;
+var display = 0;
 
-document.getElementById("numvalue").innerText = value;
+document.getElementById("numvalue").innerText = display;
+function input(){
+}
+function clear(){
+    display = 0;
+    document.getElementById("numvalue").innerText = display;
+}
+document.getElementById("clear").onclick = clear;
+document.getElementById("subtraction").onclick = sub;
+document.getElementById("addition").onclick = add;
+document.getElementById("multiplication").onclick = multiply;
+document.getElementById("division").onclick = divide;
 
-function subone(){
-    value -= 1;
-    document.getElementById("numvalue").innerText = value;
-}
-function addone(){
-    value += 1;
-    document.getElementById("numvalue").innerText = value;
-}
-document.getElementById("subtraction").onclick = subone;
-document.getElementById("addition").onclick = addone;
+document.getElementById("1").onclick = 1;
+document.getElementById("2").onclick = 2;
+document.getElementById("3").onclick = 3;
+document.getElementById("4").onclick = 4;
+document.getElementById("5").onclick = 5;
+document.getElementById("6").onclick = 6;
+document.getElementById("7").onclick = 7;
+document.getElementById("8").onclick = 8;
+document.getElementById("9").onclick = 9;
+document.getElementById("0").onclick = 0;
