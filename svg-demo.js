41a4e3916e706ddb004a7ed321856f4f568ef2d3
nakeopilavan <nakeopilavan@s.sfusd.edu>
2019-03-13 17:59:33 -0700

    update

---
+++ svg-demo.js
@@ -0,0 +1,25 @@
+var w = 600;
+var h = 600;
+
+var draw = SVG('drawing').size(w, h);
+
+function drawFlower(x,y,color){
+    var stem = draw.line(x,y,x,y+60);
+    stem.stroke({color:'green', width:5})
+    for(var i = 0; i < 8; i++){
+        var degree = i*(Math.PI/4);
+        var cx = Math.cos(degree);
+        var cy = Math.sin(degree);
+        var circle = draw.circle(20).fill('purple');
+        circle.center(x+(cx*20), y+(cy*20));
+    }
+    var mCircle = draw.circle(40).fill(color);
+    mCircle.center(x,y);
+}
+
+for(var i = 0; i < 9; i++){
+    var x = Math.random()*500+40;
+    var y = Math.random()*500+40;
+    drawFlower(x, y,'yellow');
+}
+
